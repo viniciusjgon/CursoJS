1321103764103d18126a07dc50425bcd7f371869
Vinícius Gonçalves <124836858+viniciusjgon@users.noreply.github.com>
2023-06-19 18:55:23 -0300

    finalizando o contador e criando a tabuada

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -5,15 +5,26 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
         window.alert('[ERRO] Faltando dados')
     } else { 
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= fim; c += p) {
-            res.innerHTML += `${c}`
+        if (p <= 0) {
+            window.alert ('Passo inválido! Considerando PASSO 1.')
+            p = 1
         }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            } 
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }  
+        }
+        res.innerHTML += `\u{1F3C1} `
     }
 }
