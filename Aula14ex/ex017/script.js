85a4c3164871c74f0c44abe8055d5426085be603
Vinícius Gonçalves <124836858+viniciusjgon@users.noreply.github.com>
2023-06-19 23:01:37 -0300

    iniciando tabuada com js

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,30 +1,10 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel contar!'
-        window.alert('[ERRO] Faltando dados')
-    } else { 
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0) {
-            window.alert ('Passo inválido! Considerando PASSO 1.')
-            p = 1
-        }
-        if (i < f) {
-            for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} \u{1F449} `
-            } 
-        } else {
-            for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
-            }  
-        }
-        res.innerHTML += `\u{1F3C1} `
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('POR FAVOR, DIGITE UM NÚMERO!')
+    } else {
+        let n = Number(num.value)
     }
-}
+} 
+
